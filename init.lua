2badc31bb62b89855ade34546c86b26cd010fe23
bullpiix <bernardstanleypanama@gmail.com>
2025-06-19 16:19:38 -0500

    ver3.0

---
+++ init.lua
@@ -0,0 +1,86 @@
+-- Inicia Packer
+
+--vim.cmd("colorscheme shine") -- puedes probar también: rose-pine-dawn, rose-pine-moon
+
+vim.api.nvim_set_keymap("n", "'", "<Plug>(emmet-expand-abbr)", {})
+require('boxRun')
+require('gitUI')
+require('manuSetup')
+require('treeExplorer')
+require("my_cmp_config")  -- Tu archivo cmp.lua
+require("lsp")
+
+
+vim.cmd [[packadd packer.nvim]]
+--vim.cmd('source /home/srrogu/Documentos/mymorning.vim')
+
+require('packer').startup(function(use)
+    use 'mattn/emmet-vim'
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { 'nvim-lua/plenary.nvim' }
+    }
+    use {
+      'L3MON4D3/LuaSnip',
+      requires = { 'rafamadriz/friendly-snippets' },
+    }
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'neovim/nvim-lspconfig' 
+    use {
+        'andymass/vim-matchup',
+        setup = function()
+          vim.g.matchup_matchparen_offscreen = { method = "popup" } -- opcional
+        end
+    }
+    use 'wbthomason/packer.nvim'
+    use 'hrsh7th/nvim-cmp'         -- Plugin principal de autocompletado
+    use 'hrsh7th/cmp-buffer'       -- Fuente para el buffer actual
+    use({
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          ensure_installed = { "python","html", "css", "javascript" },  -- Asegura que el parser de Python esté instalado
+          matchup = {
+            enable = true
+          },
+          highlight = {
+            enable = true,  -- Habilitar el resaltado de sintaxis
+            additional_vim_regex_highlighting = false,  -- Desactiva resaltado adicional de Vim
+          },
+          indent = { enable = false},
+        }
+      end
+    })
+
+
+    use({
+      'projekt0n/github-nvim-theme',
+      config = function()
+        require('github-theme').setup({
+          -- Configuración adicional del tema
+        })
+
+        vim.cmd('colorscheme github_light')
+
+        -- Modificar el grupo de resaltado de Folded manualmente
+        vim.api.nvim_set_hl(0, 'Folded', { fg = '#0077cc', bg = '#dddddd', italic = true }) 
+      end
+    })
+
+    use {
+      "lukas-reineke/indent-blankline.nvim",
+      config = function()
+        -- Cambia el color de la línea vertical
+        vim.api.nvim_set_hl(0, "IblIndent", { fg = "#5c6370" })
+
+        require("ibl").setup({
+          indent = { char = "│" },
+          scope = { enabled = false },
+          exclude = {
+            filetypes = { "help", "terminal", "dashboard", "lazy" },
+          },
+        })
+      end
+    }
+end)
+
---
+++ plugin/packer_compiled.lua
@@ -0,0 +1,188 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+_G._packer = _G._packer or {}
+_G._packer.inside_compile = true
+
+local time
+local profile_info
+local should_profile = false
+if should_profile then
+  local hrtime = vim.loop.hrtime
+  profile_info = {}
+  time = function(chunk, start)
+    if start then
+      profile_info[chunk] = hrtime()
+    else
+      profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+    end
+  end
+else
+  time = function(chunk, start) end
+end
+
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+  if threshold then
+    table.insert(results, '(Only showing plugins that took longer than ' .. threshold .. ' ms ' .. 'to load)')
+  end
+
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/home/manutip/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/home/manutip/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/home/manutip/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/home/manutip/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/manutip/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["emmet-vim"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/emmet-vim",
+    url = "https://github.com/mattn/emmet-vim"
+  },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
+  },
+  ["github-nvim-theme"] = {
+    config = { "\27LJ\2\nÃ\1\0\0\5\0\n\0\0186\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\3\0009\0\4\0'\2\5\0B\0\2\0016\0\3\0009\0\6\0009\0\a\0)\2\0\0'\3\b\0005\4\t\0B\0\4\1K\0\1\0\1\0\3\vitalic\2\abg\f#dddddd\afg\f#0077cc\vFolded\16nvim_set_hl\bapi\29colorscheme github_light\bcmd\bvim\nsetup\17github-theme\frequire\0" },
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
+    url = "https://github.com/projekt0n/github-nvim-theme"
+  },
+  ["indent-blankline.nvim"] = {
+    config = { "\27LJ\2\nž\2\0\0\5\0\17\0\0226\0\0\0009\0\1\0009\0\2\0)\2\0\0'\3\3\0005\4\4\0B\0\4\0016\0\5\0'\2\6\0B\0\2\0029\0\a\0005\2\t\0005\3\b\0=\3\n\0025\3\v\0=\3\f\0025\3\14\0005\4\r\0=\4\15\3=\3\16\2B\0\2\1K\0\1\0\fexclude\14filetypes\1\0\1\14filetypes\0\1\5\0\0\thelp\rterminal\14dashboard\tlazy\nscope\1\0\1\fenabled\1\vindent\1\0\3\fexclude\0\vindent\0\nscope\0\1\0\1\tchar\bâ”‚\nsetup\bibl\frequire\1\0\1\afg\f#5c6370\14IblIndent\16nvim_set_hl\bapi\bvim\0" },
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-treesitter"] = {
+    config = { "\27LJ\2\n§\2\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\vindent\1\0\1\venable\1\14highlight\1\0\2&additional_vim_regex_highlighting\1\venable\2\fmatchup\1\0\1\venable\2\21ensure_installed\1\0\4\14highlight\0\vindent\0\fmatchup\0\21ensure_installed\0\1\5\0\0\vpython\thtml\bcss\15javascript\nsetup\28nvim-treesitter.configs\frequire\0" },
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-matchup"] = {
+    after_files = { "/home/manutip/.local/share/nvim/site/pack/packer/opt/vim-matchup/after/plugin/matchit.vim" },
+    loaded = true,
+    needs_bufread = true,
+    path = "/home/manutip/.local/share/nvim/site/pack/packer/opt/vim-matchup",
+    url = "https://github.com/andymass/vim-matchup"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Setup for: vim-matchup
+time([[Setup for vim-matchup]], true)
+try_loadstring("\27LJ\2\nN\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\0\1\vmethod\npopup!matchup_matchparen_offscreen\6g\bvim\0", "setup", "vim-matchup")
+time([[Setup for vim-matchup]], false)
+time([[packadd for vim-matchup]], true)
+vim.cmd [[packadd vim-matchup]]
+time([[packadd for vim-matchup]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+try_loadstring("\27LJ\2\n§\2\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\vindent\1\0\1\venable\1\14highlight\1\0\2&additional_vim_regex_highlighting\1\venable\2\fmatchup\1\0\1\venable\2\21ensure_installed\1\0\4\14highlight\0\vindent\0\fmatchup\0\21ensure_installed\0\1\5\0\0\vpython\thtml\bcss\15javascript\nsetup\28nvim-treesitter.configs\frequire\0", "config", "nvim-treesitter")
+time([[Config for nvim-treesitter]], false)
+-- Config for: indent-blankline.nvim
+time([[Config for indent-blankline.nvim]], true)
+try_loadstring("\27LJ\2\nž\2\0\0\5\0\17\0\0226\0\0\0009\0\1\0009\0\2\0)\2\0\0'\3\3\0005\4\4\0B\0\4\0016\0\5\0'\2\6\0B\0\2\0029\0\a\0005\2\t\0005\3\b\0=\3\n\0025\3\v\0=\3\f\0025\3\14\0005\4\r\0=\4\15\3=\3\16\2B\0\2\1K\0\1\0\fexclude\14filetypes\1\0\1\14filetypes\0\1\5\0\0\thelp\rterminal\14dashboard\tlazy\nscope\1\0\1\fenabled\1\vindent\1\0\3\fexclude\0\vindent\0\nscope\0\1\0\1\tchar\bâ”‚\nsetup\bibl\frequire\1\0\1\afg\f#5c6370\14IblIndent\16nvim_set_hl\bapi\bvim\0", "config", "indent-blankline.nvim")
+time([[Config for indent-blankline.nvim]], false)
+-- Config for: github-nvim-theme
+time([[Config for github-nvim-theme]], true)
+try_loadstring("\27LJ\2\nÃ\1\0\0\5\0\n\0\0186\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\3\0009\0\4\0'\2\5\0B\0\2\0016\0\3\0009\0\6\0009\0\a\0)\2\0\0'\3\b\0005\4\t\0B\0\4\1K\0\1\0\1\0\3\vitalic\2\abg\f#dddddd\afg\f#0077cc\vFolded\16nvim_set_hl\bapi\29colorscheme github_light\bcmd\bvim\nsetup\17github-theme\frequire\0", "config", "github-nvim-theme")
+time([[Config for github-nvim-theme]], false)
+
+_G._packer.inside_compile = false
+if _G._packer.needs_bufread == true then
+  vim.cmd("doautocmd BufRead")
+end
+_G._packer.needs_bufread = false
+
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
